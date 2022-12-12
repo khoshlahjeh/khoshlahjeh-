019cd9d6fc79cd1392174444b2d85d81461a75bb
khoshlahjeh <119805112+khoshlahjeh@users.noreply.github.com>
2022-12-12 22:14:48 +0330

    Add files via upload

---
+++ py.t1.py
@@ -0,0 +1,16 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> python='hani khoshlahjeh motlag'
+>>> n_h=python.split()
+>>> n_k=len(python)
+>>> print(len(n_h))
+3
+>>> print(n_k)
+23
+>>> print(python.replace('h','m'))
+mani kmosmlamjem motlag
+>>> 
+>>> python='Haniyeh.zahra.maryam.mina.ati'
+>>> print(python.count('.'))
+4
+>>> 
